666567dedad7f4360706c3fe0874ea7e13959f12
Marius Bråthen <marius.brathen@me.com>
2022-03-24 23:45:24 +0100

    Edit scoreboard more according to the sketches

--- src/pages/highscores/index.tsx
+++ src/pages/highscores/index.tsx
@@ -4,6 +4,8 @@ import css from './Highscores.module.scss';
 import { fetchRealTimeScoreList } from '../../helpers';
 import { GameStateContext } from '../../contexts/GameStateContext';
 import { GameStateActionType } from '../../enums/GameStateActionTypes';
+import Display from 'components/display/Display';
+import { ReactComponent as TetrisHeader } from '../../svg/tetrisHeader.svg';
 
 const Highscores = (): ReactElement => {
   const { gameState, gameDispatch } = useContext(GameStateContext);
@@ -21,13 +23,16 @@ const Highscores = (): ReactElement => {
   }, []);
 
   return (
-    <div className={css.Highscores}>
-      <h1>Topp 20</h1>
-      <p>{gameState.scoreList.length} spill registrert</p>
-
-      <ol>
+    <div className={css.container}>
+      <TetrisHeader className={css.TetrisHeader} />
+      <p className={css.GamesPlayed}>
+        {gameState.scoreList.length} spill registrert
+      </p>
+      <ol className={css.ScoreBoardList}>
         {gameState.scoreList.slice(0, 20).map((score, index) => (
-          <li key={index}>{score.score}</li>
+          <li key={index}>
+            <Display content={score.score} style={{ fontSize: '25px' }} />
+          </li>
         ))}
       </ol>
     </div>

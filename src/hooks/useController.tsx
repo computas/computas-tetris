0d92083635464f2aff8e0f6da35dce8658ce2eb2
Ole Christian Helset <ole.christian.helseth@computas.com>
2022-04-04 11:38:05 +0200

    Resolved issue where space was disabled in the input fields on the score page

--- src/hooks/useController.tsx
+++ src/hooks/useController.tsx
@@ -17,7 +17,11 @@ export const useController = (): [
   const [rotatePressState, setRotatePressState] = useState(false);
 
   const handleKeyPressed = (event: { key: string }, state: GameState): void => {
-    if (state.gameOver || state.startScreen) {
+    if (state.gameOver) {
+      return;
+    }
+
+    if (state.startScreen) {
       if (event.key === ' ' && !downPressState) {
         setDownPressState(true);
       }

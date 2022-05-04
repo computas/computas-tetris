c68d49234a5ad116627f26299d5794c4f271027b
Ole Christian Helset <ole.christian.helseth@computas.com>
2022-05-04 12:09:10 +0200

    Added music off icon as well

--- src/components/button/MusicButton.tsx
+++ src/components/button/MusicButton.tsx
@@ -2,12 +2,13 @@ import React, { ReactElement, useContext } from 'react';
 
 import css from './Button.module.scss';
 import Button, { ButtonSize, ButtonVariant } from './Button';
-import { ReactComponent as MusicOn } from '../../svg/volume.svg';
-import { GameSettingsStateActionType } from '../../enums/GameSettingsStateActionTypes';
 import { GameSettingsContext } from '../../contexts/GameSettingsContext';
+import { GameSettingsStateActionType } from '../../enums/GameSettingsStateActionTypes';
+import { ReactComponent as MusicOff } from '../../svg/volume-off.svg';
+import { ReactComponent as MusicOn } from '../../svg/volume-on.svg';
 
 const MusicButton = (): ReactElement => {
-  const { settingsDispatch } = useContext(GameSettingsContext);
+  const { gameSettings, settingsDispatch } = useContext(GameSettingsContext);
 
   const toggleMusic = (): void => {
     settingsDispatch({ type: GameSettingsStateActionType.ToggleMusic });
@@ -21,7 +22,7 @@ const MusicButton = (): ReactElement => {
         variant={ButtonVariant.Clear}
         onClick={toggleMusic}
       >
-        <MusicOn />
+        {gameSettings.playMusic ? <MusicOn /> : <MusicOff />}
       </Button>
     </div>
   );
